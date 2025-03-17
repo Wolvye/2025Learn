adf938dc85fb5aaf17514eab693eb0be235254b4
Wolvye <alainsoltau@gmail.com>
2025-03-17 09:36:43 +0100

    Add project files.

---
+++ first/MyApp/MyApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie heißt du? ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Wie alt bist du?");
+            string age = Console.ReadLine();
+            int birthYear = 2025 -int.Parse(age);
+            Console.WriteLine("Dein name ist" + " " + name +" "+ "und du im Jahre"+" " + birthYear +   " " +"geboren");
+        }
+    }
+}

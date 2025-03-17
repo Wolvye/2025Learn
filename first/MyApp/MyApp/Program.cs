9af9f01f3adddcd2df1b70d0a96dbe7b3a39e431
Wolvye <alainsoltau@gmail.com>
2025-03-17 11:50:48 +0100

    Update

--- first/MyApp/MyApp/Program.cs
+++ first/MyApp/MyApp/Program.cs
@@ -3,14 +3,71 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             Console.WriteLine("Wie heißt du? ");
             string name = Console.ReadLine();
 
             Console.WriteLine("Wie alt bist du?");
             string age = Console.ReadLine();
-            int birthYear = 2025 -int.Parse(age);
-            Console.WriteLine("Dein name ist" + " " + name +" "+ "und du im Jahre"+" " + birthYear +   " " +"geboren");
+            int birthYear = 2025 - int.Parse(age);
+            Console.WriteLine("Dein name ist" + " " + name + " " + "und du im Jahre" + " " + birthYear + " " + "geboren");
+           
+
+            //Arrays
+
+
+            int[] numbers = new int[5];
+            numbers[0] = 100;
+            numbers[1] = 200;
+            numbers[2] = 300;
+            numbers[3] = 800;
+            numbers[4] = 400;
+
+            Array.Sort(numbers);
+
+            //Schleifen
+           
+            int result = 0;
+            foreach (int n in numbers)
+            {
+                result += n;
+                
+            }
+            Console.WriteLine(result);
+
+            for (int i = 0; i<numbers.Length; i++) 
+            {
+                result += numbers[i];
+            }
+            Console.WriteLine(result);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            for (int i = 2; i <= 20; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+            for (int i = 10; i>=1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            
+            int[] numbers2 = new int[3] { 5, 10, -4 };
+            int i = 0;
+            while (i < numbers2.Length)
+            {
+              
+                Console.WriteLine(numbers2[i]);
+                i++;
+        }
+             */
+
+
         }
     }
 }

3246b8b7ec3f675f93cfdf195e0274b928d6cfe0
Wolvye <alainsoltau@gmail.com>
2025-03-24 13:29:31 +0100

    warm ups

--- first/thrid/thrid/Program.cs
+++ first/thrid/thrid/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.Design;
 
 namespace thrid
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*
 
             //Anwendung: Todo List
             // was brauche ich ?
@@ -19,7 +20,8 @@ namespace thrid
             {
                 Console.WriteLine("Please enter a command (add, view,delete,quit): ");
                 command = Console.ReadLine().ToLower();
-                if (command == "add")
+                
+            if (command == "add")
                 {
                     if (currentTaskCount < todoList.Length)
                     {
@@ -65,7 +67,85 @@ namespace thrid
                 {
                     Console.WriteLine("Good Bye");
                 }
+            
+
+            //was brauche ich ?
+            // Add,View,Delete,Quit
+            //ohne stützräder
+
+            string[] ToDoList = new string[5];
+            string command = "";
+            int zähler = 0;
+
+            while (command != "quit")
+            {
+                Console.WriteLine("Gib einer der folgenden Komandos ein Add,View,Delete,Quit");
+                command = Console.ReadLine().ToLower();
+
+
+                if (command == "add")
+                {
+                    if (zähler <= 5)
+                    {
+                        Console.WriteLine("Bitte gib was in deine Liste ein");
+                        string aufgabe = Console.ReadLine();
+                        ToDoList[zähler] = aufgabe;
+                        zähler++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Deine ToDoListe ist voll");
+                    }
+
+                }
+                else if (command == "view")
+                {
+                    Console.WriteLine("Deine Aktuelle Liste: ");
+
+                    for (int i = 0; i <= zähler; i++)
+                    {
+                        Console.WriteLine(ToDoList[i]);
+                    }
+                }
+
+                else if (command == "delete")
+                {
+                    Console.WriteLine("was soll gelöscht werden?");
+                    int aufgabenZähler = int.Parse(Console.ReadLine()) - 1;
+
+                    if (aufgabenZähler < 0 || aufgabenZähler >= zähler)
+                    {
+                        Console.WriteLine("Invalid task number.");
+                    }
+                    else
+                    {
+                        for (int i = aufgabenZähler; i < zähler - 1; i++)
+                        {
+                            ToDoList[i] = ToDoList[i + 1];
+                        }
+                        ToDoList[zähler - 1] = null;
+                        zähler--;
+                        Console.WriteLine("Task removed.");
+                    }
+                }
+
             }
+            if (command == "quit")
+            {
+                Console.WriteLine("Ciao Kakao!");
+            }
+
+
+            */
+            //Reserviere ein Wort - dreh es um!
+            /*
+            string rueckwaertsWort = "";
+            Console.WriteLine("Bitte gib etwas ein, das wird rückwärts zurück gegeben");
+            rueckwaertsWort = Console.ReadLine();
+            rueckwaertsWort = String.Concat(rueckwaertsWort.Reverse());
+            Console.WriteLine(rueckwaertsWort);
+            */
+
         }
     }
 }

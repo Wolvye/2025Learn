b5397085007cec42e1a6012d7d14498cd7fe8e2f
Wolvye <alainsoltau@googlemail.com>
2025-04-08 11:10:06 +0200

    First

---
+++ seven/lesson90/SwitchesEnum/SwitchesEnum/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switches
+            Console.WriteLine("Bitte geben Sie einen Wochentag ein (1-7): ");
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day == 1)
+            {
+            }
+
+        }
+    }
+}

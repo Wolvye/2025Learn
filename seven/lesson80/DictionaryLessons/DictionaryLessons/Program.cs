3e4c296b2a94de65b1b32c861803ec77358b37e2
Wolvye <alainsoltau@googlemail.com>
2025-04-08 11:02:16 +0200

    First

---
+++ seven/lesson80/DictionaryLessons/DictionaryLessons/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Aufgabe: Wie oft kommt jede Zahl vor.
+            //Versuche es mit einem Dictionary zu lösen
+
+            int[] numbers = new int[10]
+                {
+                    10,20,40,90,50,10,20,40,90,50
+                };
+
+            Dictionary<int,int> numbersCount = new Dictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                numbersCount.Add(number, 1);
+            }
+
+        }
+    }
+}

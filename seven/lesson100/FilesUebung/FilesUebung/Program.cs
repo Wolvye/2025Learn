7b6c8e1aa7a4601258b964bcd9f9c38dec2247fd
Wolvye <alainsoltau@googlemail.com>
2025-04-09 13:08:34 +0200

    Update

---
+++ seven/lesson100/FilesUebung/FilesUebung/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO; // IO = Input Output
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Create File
+            string path = "test.txt";
+            //    //File.Create(path);
+
+            //    // Read File
+            //   string content =  File.ReadAllText(path);
+            //    Console.WriteLine(content);
+
+            //Copy File mit kleiner Absicherung
+            //if (File.Exists(path))
+            //{
+            //    string destinationPath = "Test2.txt";
+            //    File.Copy(path, destinationPath);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can not find file");
+
+            //    FileStream fs = File.Create(path);
+            //    fs.Close();
+
+            //    Console.WriteLine("Path created");
+            //    File.WriteAllText(path, "Nix Los");
+            //}
+
+            //Append anfügen
+            //if (File.Exists(path))
+            //{
+            //    File.AppendAllText(path, "I'm Batman!");
+            //}
+
+            //Delete
+            if (File.Exists(path))
+            { 
+                File.Delete(path);
+            }
+
+
+        }
+    }
+}

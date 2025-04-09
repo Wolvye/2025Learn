bf0b87390422491f0fc662787aabd585b556e407
Wolvye <alainsoltau@googlemail.com>
2025-04-09 12:23:39 +0200

    Finish

--- seven/lesson90/NastedLoops/NastedLoops/Program.cs
+++ seven/lesson90/NastedLoops/NastedLoops/Program.cs
@@ -9,11 +9,17 @@
             products.Add(new Product("Pfanne", new List<int> { 1, 2, 4 }));
             products.Add(new Product("Grill", new List<int> { 5, 4, 3 }));
             products.Add(new Product("Topf", new List<int> { 5, 2, 5 }));
+            products.Add(new Product("Herd", new List<int> { 6, 7, 6 }));
 
 
             for (int i = 0; i < products.Count; i++)
             {
-                Console.WriteLine("Produkt " + products[i].Name);
+                if (products[i].Name == "Grill") //hier wird der Grill übersprungen
+                {
+                    continue;
+                }
+
+                    Console.WriteLine("Produkt " + products[i].Name);
                 for (int j = 0; j < products[i].Ratings.Count; j++)
                 {
                     Console.WriteLine($"Das {products[i].Ratings[j]} hat eine Sternebewertung");

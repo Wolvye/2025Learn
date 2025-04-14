e161448a65018c140abf839b30208c0967000c7e
Wolvye <alainsoltau@googlemail.com>
2025-04-14 10:49:43 +0200

    Start New Projekt

---
+++ BioMarktUebung/BioMarktUebung/BioMarktUebung/LoadingScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BioMarktUebung
+{
+    public partial class LoadingScreen : Form
+    {
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
